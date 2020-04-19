ab79798da97ac16787e6486188996625bb8c1a2f
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-04-19 21:33:36 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#DMM WEBCAMPコンテンツ【JavaScriptを学ぼう】
+DMM WEBCAMPの学習コンテンツJavaScriptを学ぼうの研修課題です。
+
+## 使い方
+htmlファイルをダブルクリックすると動作が確認できます。
---
+++ ３章/関数　単数/app.js
@@ -0,0 +1,9 @@
+var alertString;
+alertString = addString("WebCamp");
+
+function addString(strA){
+	var addStr = "Hello " + strA;
+	return addStr;
+}
+
+alert(alertString);
